7188e864601be226bc367dfd59f502d8e03f6c6a
Fernando Jasso <jasso.toscano@gmail.com>
2023-06-09 09:15:23 -0600

    tarea1:bug:

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config(); //configuro las env
+const app = require('./app');
+const { db } = require('./database/config');
+
+//configuracion de la base de datos
+db.authenticate()
+.then(() => console.log('Database authenticated... 🐢'))
+.catch((err) => console.log(err));
+
+db.sync()
+.then(() => console.log('Database synced... 🦜'))
+.catch((err) => console.log(err));
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`server running on PORT ${PORT}...🐣`)
+});
